a97ed690c4793d96638625152bfdae75d14f31b1
Pedro Shin <pedromaschio.shin@gmail.com>
2026-04-03 21:41:42 -0300

    fix: separate CAPI call from pixel Script to fix appendChild error

--- src/components/layout/AnalyticsScripts.tsx
+++ src/components/layout/AnalyticsScripts.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { usePathname } from "next/navigation";
 import Script from "next/script";
 import { Analytics } from "@vercel/analytics/next";
@@ -10,6 +10,13 @@ import { trackPageViewCAPI } from "@/lib/tracking";
 export default function AnalyticsScripts() {
   const pathname = usePathname();
   const capiSent = useRef(false);
+
+  useEffect(() => {
+    if (!capiSent.current && process.env.NEXT_PUBLIC_FB_PIXEL_ID) {
+      capiSent.current = true;
+      trackPageViewCAPI();
+    }
+  }, []);
 
   if (pathname?.startsWith("/admin")) {
     return null;
@@ -44,12 +51,6 @@ export default function AnalyticsScripts() {
           <Script
             id="fb-pixel"
             strategy="afterInteractive"
-            onLoad={() => {
-              if (!capiSent.current) {
-                capiSent.current = true;
-                trackPageViewCAPI();
-              }
-            }}
             dangerouslySetInnerHTML={{
               __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','${process.env.NEXT_PUBLIC_FB_PIXEL_ID}');fbq('track','PageView');`,
             }}
